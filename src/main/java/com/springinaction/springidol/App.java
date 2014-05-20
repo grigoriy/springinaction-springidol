f4f9a2a1c795cebf5085f3ea4c67f952fbc04e13
Grigoriy Alexeev <grigoriyalexeev@gmail.com>
2014-05-20 11:12:06 +0400

    2.2.2

--- src/main/java/com/springinaction/springidol/App.java
+++ src/main/java/com/springinaction/springidol/App.java
@@ -2,7 +2,11 @@ package com.springinaction.springidol;
 
 import com.springinaction.springidol.exceptions.PerformanceException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -12,13 +16,25 @@ public class App {
 
     public static void main( String[] args ) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("application-context.xml");
 
-        Performer performer = (Performer) context.getBean("poeticDuke");
+        List<Performer> performers = new ArrayList<>();
+        performers.add((Performer) context.getBean("duke"));
+        performers.add((Performer) context.getBean("poeticDuke"));
+        performers.add((Performer) context.getBean("kenny"));
+
+        Stage stage = (Stage) context.getBean("stage");
+        Auditorium auditorium = (Auditorium) context.getBean("auditorium");
         try {
-            performer.perform();
+            for (Performer performer : performers) {
+                performer.perform();
+            }
+
         } catch (PerformanceException e) {
             e.printStackTrace();
         }
+
+        ((ConfigurableApplicationContext) context).close();
     }
 }
